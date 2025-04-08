4dd82e30b36356392c790b472df24be04af14dba
bernardommedeiros <161090624+bernardommedeiros@users.noreply.github.com>
2025-04-08 17:23:57 -0300

    rotas para paginas html

--- projetos/views.py
+++ projetos/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# página principal - acionada pela requisição do user
+def index(request):
+    # caminho para arquivo html da primeira pagina
+    return render(request, 'projetos/index.html')
